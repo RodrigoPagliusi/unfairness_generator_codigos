6ebc34f9e93a9081a5da4152771fe76d56c4cca9
Rodrigo Pereira Pagliusi <rodrigo.pagliusi@altageotecnia.com>
2024-12-04 18:02:59 -0300

    'atual'

--- model_trainer.py
+++ model_trainer.py
@@ -35,8 +35,7 @@ class ModelTrainer:
         hyperparams_path = os.path.join("Codigos", self.config.get(
             'models', {}).get('hyperparameters_path', 'config_hyperparameters.json'))
         if not os.path.isfile(hyperparams_path):
-            logging.error(f"Hyperparameters file not found at {
-                          hyperparams_path}")
+            logging.error(f"Hyperparameters file not found at {hyperparams_path}")
             raise FileNotFoundError(
                 f"Hyperparameters file not found at {hyperparams_path}")
 
@@ -61,8 +60,7 @@ class ModelTrainer:
         )
         if os.path.isfile(model_filename):
             model = joblib.load(model_filename)
-            logging.info(f"Loaded existing {
-                         model_identifier} model from {model_filename}")
+            logging.info(f"Loaded existing {model_identifier} model from {model_filename}")
             return model
 
         if tune_hyperparameters:
@@ -115,8 +113,7 @@ class ModelTrainer:
                 # random_state=42
             )
             search.fit(X_train, y_train)
-            logging.info(f"Best hyperparameters for {
-                         model_name}: {search.best_params_}")
+            logging.info(f"Best hyperparameters for {model_name}: {search.best_params_}")
             best_model = search.best_estimator_
             return best_model
         elif model_name in ["MLPClassifier", "SVC"]:
@@ -132,8 +129,7 @@ class ModelTrainer:
             study = optuna.create_study(direction='maximize', sampler=sampler)
             study.optimize(objective, n_trials=num_trials)
 
-            logging.info(f"Best hyperparameters for {
-                         model_name}: {study.best_params}")
+            logging.info(f"Best hyperparameters for {model_name}: {study.best_params}")
 
             best_model = self.initialize_model_with_params(
                 model_name, study.best_params)
@@ -141,8 +137,7 @@ class ModelTrainer:
 
             return best_model
         elif model_name == "GaussianNB":
-            logging.info(f"No hyperparameters to tune for {
-                         model_name}. Training default model.")
+            logging.info(f"No hyperparameters to tune for {model_name}. Training default model.")
             model = self.initialize_model(model_name)
             model.fit(X_train, y_train)
             return model
@@ -161,10 +156,8 @@ class ModelTrainer:
             else:
                 return param_dist
         else:
-            logging.error(f"Hyperparameters for model '{
-                          model_name}' are not defined in config_hyperparameters.json.")
-            raise ValueError(f"Hyperparameters for model '{
-                             model_name}' are not defined.")
+            logging.error(f"Hyperparameters for model '{model_name}' are not defined in config_hyperparameters.json.")
+            raise ValueError(f"Hyperparameters for model '{model_name}' are not defined.")
 
     def initialize_model_with_trial(self, trial, model_name):
 
@@ -194,16 +187,12 @@ class ModelTrainer:
                         param_options['choices']
                     )
                 else:
-                    logging.error(f"Unsupported parameter type '{
-                                  param_type}' for {param_name}.")
-                    raise ValueError(f"Unsupported parameter type '{
-                                     param_type}' for {param_name}.")
+                    logging.error(f"Unsupported parameter type '{param_type}' for {param_name}.")
+                    raise ValueError(f"Unsupported parameter type '{param_type}' for {param_name}.")
             return self.initialize_model_with_params(model_name, params)
         else:
-            logging.error(f"Hyperparameters for model '{
-                          model_name}' are not defined in config_hyperparameters.json.")
-            raise ValueError(f"Hyperparameters for model '{
-                             model_name}' are not defined.")
+            logging.error(f"Hyperparameters for model '{model_name}' are not defined in config_hyperparameters.json.")
+            raise ValueError(f"Hyperparameters for model '{model_name}' are not defined.")
 
     def initialize_model_with_params(self, model_name, params):
 
